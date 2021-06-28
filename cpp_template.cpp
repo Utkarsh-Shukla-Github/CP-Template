e5e568cd604cd6609a24ce9a547bb7b34436934c
Utkarsh Shukla <61664827+utkarsh314@users.noreply.github.com>
2021-06-28 22:11:26 +0530

    Updated cpp_template.cpp

--- cpp_template.cpp
+++ cpp_template.cpp
@@ -1,56 +1,26 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define endl ("\n")
+
+#define endl ('\n')
 #define pi (3.141592653589)
 #define mod 1e9 + 7
 #define int long long
 #define float double
-#define pb push_back
-#define mp make_pair
-#define ff first
-#define ss second
-#define all(c) cc.begin(), c.end()
-#define mini(a, b, c) min(c, min(a, b))
-#define mini(a, b, c, d) min(d, min(c, min(a, b)))
-#define rrep(i, n) for (int i = n - 1; i >= 0; i--)
-#define rep(i, n) for (int i = 0; i < n; i++)
+#define test int t; cin>>t; while(t--)
+#define init(arr,val) memset(arr,val,sizeof(arr))
 #define fast ios_base::sync_with_stdio(false), cin.tie(nullptr), cout.tie(nullptr);
+#define ret return 0;
 
-// Maths functions
-bool isPrime(int n)
-{
-    // Corner case
-    if (n <= 1)
-        return false;
+void solve() {
 
-    // Check from 2 to n-1
-    for (int i = 2; i < n; i++)
-        if (n % i == 0)
-            return false;
-
-    return true;
-}
-
-// String Function
-bool ispalindrome(const string &s)
-{
-    return s == string(s.rbegin(), s.rend());
-}
-
-// Solve Function
-void solve()
-{
+    /*Write the Solution Here*/
+     
 }
 
 int32_t main()
 {
     fast
-
-    int t = 1;
-    cin >> t;
-    while (t--)
-    {
+    test
         solve();
-    }
-    return 0;
+    ret
 }
